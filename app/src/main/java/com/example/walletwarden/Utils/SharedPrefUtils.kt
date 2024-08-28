4c71786661bd4f569ed69e2b24dc0463a22135a7
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-08-28 22:47:24 +0530

    Added saveUserData

--- app/src/main/java/com/example/walletwarden/Utils/SharedPrefUtils.kt
+++ app/src/main/java/com/example/walletwarden/Utils/SharedPrefUtils.kt
@@ -1,2 +1,13 @@
 package com.example.walletwarden.Utils
 
+import android.content.Context
+import android.content.SharedPreferences
+
+fun saveUserData(context: Context, name: String, balance:Int) {
+    val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+    val editor = sharedPreferences.edit()
+    editor.putString("userName", name)
+    editor.putInt("userBalance",balance)
+    editor.apply()
+}
