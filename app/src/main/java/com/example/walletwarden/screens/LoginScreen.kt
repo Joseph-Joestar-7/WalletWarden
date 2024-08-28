ba338dba7602f9af4f693cb7ea9925f3b3fcb9f1
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-08-28 22:51:32 +0530

    Added a box and column to loginscreen

--- app/src/main/java/com/example/walletwarden/screens/LoginScreen.kt
+++ app/src/main/java/com/example/walletwarden/screens/LoginScreen.kt
@@ -1,2 +1,24 @@
 package com.example.walletwarden.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+        ) {
+        }
+    }
+}
