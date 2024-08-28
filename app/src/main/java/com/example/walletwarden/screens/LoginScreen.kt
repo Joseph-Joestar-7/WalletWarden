799662a1d9e7566a236b5ba0035613b076e36bd8
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-08-28 23:06:57 +0530

    Added a button to login screen

--- app/src/main/java/com/example/walletwarden/screens/LoginScreen.kt
+++ app/src/main/java/com/example/walletwarden/screens/LoginScreen.kt
@@ -1,11 +1,14 @@
 package com.example.walletwarden.screens
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,10 +19,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
 fun LoginScreen() {
+    val context: Context = LocalContext.current
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -29,7 +34,7 @@ fun LoginScreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             var userName by remember { mutableStateOf("") }
-            val userBalance by remember { mutableIntStateOf(0) }
+            var userBalance by remember { mutableIntStateOf(0) }
             OutlinedTextField(value = userName,
                 onValueChange ={ userName=it },
                 label={Text(text = "Need your username")},
@@ -37,6 +42,21 @@ fun LoginScreen() {
                 keyboardType = KeyboardType.Text
             ),
             modifier = Modifier.fillMaxWidth())
+            OutlinedTextField(value = userBalance.toString(),
+                onValueChange ={ userBalance=it.toInt() },
+                label={Text(text = "Need your current balance")},
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number
+                ),
+                modifier = Modifier.fillMaxWidth())
+            Button(onClick ={if(userName.isBlank()||userBalance==0)
+                Toast.makeText(context, "Atleast set up the account properly", Toast.LENGTH_SHORT).show()
+                else {
+
+            } })
+            {
+
+            }
         }
     }
 }
