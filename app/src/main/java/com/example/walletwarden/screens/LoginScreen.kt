8b09617408dbeaf87998d467885ee56c93e4ac1e
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-08-30 20:22:05 +0530

    Added a surface to Login

--- app/src/main/java/com/example/walletwarden/screens/LoginScreen.kt
+++ app/src/main/java/com/example/walletwarden/screens/LoginScreen.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.RememberObserver
@@ -23,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.walletwarden.utils.saveUserData
@@ -30,55 +33,59 @@ import com.example.walletwarden.utils.saveUserData
 @Composable
 fun LoginScreen(navController: NavController) {
     val context: Context = LocalContext.current
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
+    Surface {
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .padding(16.dp,16.dp),
+            contentAlignment = Alignment.Center
         ) {
-            var userName by remember { mutableStateOf("") }
-            var userBalance by remember { mutableIntStateOf(0) }
-            var userWalletBalance by remember { mutableIntStateOf(0) }
-            OutlinedTextField(value = userName,
-                onValueChange ={ userName=it },
-                label={Text(text = "Need your username")},
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Text
-            ),
-            modifier = Modifier.fillMaxWidth())
-            OutlinedTextField(value = userBalance.toString(),
-                onValueChange ={ userBalance=it.toInt() },
-                label={Text(text = "Need your current balance")},
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number
-                ),
-                modifier = Modifier.fillMaxWidth())
-            OutlinedTextField(value = userWalletBalance.toString(),
-                onValueChange ={ userWalletBalance=it.toInt() },
-                label={Text(text = "Need your current wallet balance")},
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number
-                ),
-                modifier = Modifier.fillMaxWidth())
-            Button(onClick ={if(userName.isBlank()||userBalance==0)
-                Toast.makeText(context, "Atleast set up the account properly", Toast.LENGTH_SHORT).show()
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                var userName by remember { mutableStateOf("") }
+                var userBalance by remember { mutableIntStateOf(0) }
+                var userWalletBalance by remember { mutableIntStateOf(0) }
+                OutlinedTextField(value = userName,
+                    onValueChange ={ userName=it },
+                    label={Text(text = "Need your username")},
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Text
+                    ),
+                    modifier = Modifier.fillMaxWidth())
+                OutlinedTextField(value = userBalance.toString(),
+                    onValueChange ={ userBalance=it.toInt() },
+                    label={Text(text = "Need your current balance")},
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    ),
+                    modifier = Modifier.fillMaxWidth())
+                OutlinedTextField(value = userWalletBalance.toString(),
+                    onValueChange ={ userWalletBalance=it.toInt() },
+                    label={Text(text = "Need your current wallet balance")},
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    ),
+                    modifier = Modifier.fillMaxWidth())
+                Button(onClick ={if(userName.isBlank()||userBalance==0)
+                    Toast.makeText(context, "Atleast set up the account properly", Toast.LENGTH_SHORT).show()
                 else {
                     saveUserData(context,name=userName, balance = userBalance,
                         wBalance = userWalletBalance)
-                navController.navigate(Screen.Home.route){
-                    popUpTo(Screen.Login.route) {
-                        inclusive = true
+                    navController.navigate(Screen.Home.route){
+                        popUpTo(Screen.Login.route) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
                     }
-                    launchSingleTop = true
+                } })
+                {
+                    Text(text = "Save and Continue")
                 }
-            } })
-            {
-                Text(text = "Save and Continue")
             }
         }
     }
+
 }
 
 @Preview
