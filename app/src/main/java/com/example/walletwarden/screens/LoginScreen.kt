2d922e856e5dcced875b886305bf5ba780a9c34b
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-08-30 20:19:21 +0530

    Added Preview in Login

--- app/src/main/java/com/example/walletwarden/screens/LoginScreen.kt
+++ app/src/main/java/com/example/walletwarden/screens/LoginScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.RememberObserver
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -21,7 +22,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.walletwarden.utils.saveUserData
 
 @Composable
@@ -76,4 +79,11 @@ fun LoginScreen(navController: NavController) {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun LoginPrev()
+{
+    LoginScreen(navController = rememberNavController())
 }
